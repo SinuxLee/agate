0e9cf3fa73af67197373c668fa528287e0b73639
helinlin <linlin.he@centurygame.com>
2021-09-02 19:36:25 +0800

    mongo 增加upsertone接口 以及测试用例

--- pkg/infra/mongo/client_test.go
+++ pkg/infra/mongo/client_test.go
@@ -12,8 +12,8 @@ var conf = &Config{
 	MaxPoolSize: 100,
 	MinPoolSize: 100,
 	MaxIdleTime: 60,
-	UserName:    "test",
-	Password:    "test",
+	UserName:    "",
+	Password:    "",
 	Database:    "test",
 }
 
@@ -27,7 +27,38 @@ func TestNewClient(t *testing.T) {
 		t.Fatal(err.Error())
 	}
 	data := &BsonData{}
-	if err := cli.FindOne(context.Background(), "libz", bson.M{"name": "libz"}, data); err != nil {
+	if err := cli.FindOne(context.Background(), "libz", bson.M{"name": "libz"}, data); err == nil {
 		t.Fatal("failed to find")
+	}
+
+	insertData := bson.M{
+		"$set": bson.M{"name": "ffa"},
+	}
+
+	if err = cli.UpsertOne(context.Background(), "libz", bson.M{"name": "ffa"}, insertData); err != nil {
+		t.Fatal("failed to upsert")
+	}
+
+	if err := cli.FindOne(context.Background(), "libz", bson.M{"name": "ffa"}, data); err != nil {
+		t.Fatal("failed to find")
+	}
+
+	if data.Name != "ffa" {
+		t.Fatalf("data.Name is %s, not ffa", data.Name)
+	}
+
+	insertData = bson.M{
+		"$set": bson.M{"name": "family"},
+	}
+	if err = cli.UpsertOne(context.Background(), "libz", bson.M{"name": "family"}, insertData); err != nil {
+		t.Fatal("failed to upsert")
+	}
+
+	if err := cli.FindOne(context.Background(), "libz", bson.M{"name": "family"}, data); err != nil {
+		t.Fatal("failed to find")
+	}
+
+	if data.Name != "family" {
+		t.Fatalf("data.Name is %s, not family", data.Name)
 	}
 }
