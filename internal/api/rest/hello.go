e0952b23a5def151a13bacb2e9145872d4a19927
libz <libingzhi2011@163.com>
2022-09-13 19:05:53 +0800

    chore: enrich swagger

--- internal/api/rest/hello.go
+++ internal/api/rest/hello.go
@@ -9,13 +9,15 @@ import (
 // Hello godoc
 // @Summary 问候
 // @Tags Hello
-// @Description get greet by name
+// @Description 这里写一大段描述
+// @Description 还支持多行
 // @Accept json
 // @Produce json
-// @Param name path string true "昵称" default(libz)
-// @Param code query string true "区服编号" default(1001)
-// @Param Content-Type header string true "数据格式" default(application/json)
-// @Success 200 {object} internal.Response{data=internal.HelloRsp} "响应体"
+// @Param 	name 			path 	string 		true 	"昵称"			default(libz)
+// @Param 	code 			query 	string 		true 	"区服编号"		default(1001)
+// @Param 	Content-Type 	header 	string 		true 	"数据格式" default(application/json)
+// @Param	body			body	object{name=string,age=int}		true	"测试数据" default({"name":"libz", "age":10})
+// @Success 200				{object}	object{code=int,message=string,data=internal.HelloRsp} "响应体"
 // @Router /v1/hello/{name} [get]
 func (c *restHandler) Hello(ctx *gin.Context) {
 	data, err := c.useCase.Hello(ctx.Request.Context(), ctx.Param("name"))
